183399c8bf884e8b71a3f513391d78716aca5c34
dvyas98 <dhimant92@gmail.com>
2020-10-12 00:14:50 -0400

    New

---
+++ Assets/Project/Scripts/Gameplay/SFXController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    public AudioClip shootsfx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayShootSound()
+    {
+        this.GetComponent<AudioSource>().PlayOneShot(shootsfx);
+    }
+}
