9af7da9c9692538dde3dc77c3525b4e26f9396f9
dvyas98 <dhimant92@gmail.com>
2020-10-12 03:34:24 -0400

    Added Player Profile and Level Generation.

---
+++ Assets/Project/Scripts/UI/PlayerProfile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProfile : MonoBehaviour
+{
+    public GameObject Profile;
+
+    void Start()
+    {
+        Profile.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Profile.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            Profile.SetActive(false);
+        }
+    }
+}
